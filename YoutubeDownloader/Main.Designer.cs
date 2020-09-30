67a4de9736ecd70a4d963919502eba607958e769
Jamuel <jammmg26@gmail.com>
2020-09-30 10:57:53 +0800

    initial commit

---
+++ YoutubeDownloader/Main.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace YoutubeDownloader
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.Label();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtUrl.Multiline = true;
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(488, 135);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(12, 180);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(458, 33);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "DOWNLOAD";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.AutoSize = true;
+            this.txtStatus.Location = new System.Drawing.Point(12, 164);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(10, 13);
+            this.txtStatus.TabIndex = 2;
+            this.txtStatus.Text = "-";
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(476, 180);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(34, 33);
+            this.btnAbout.TabIndex = 3;
+            this.btnAbout.Text = "?";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 215);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtUrl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Youtube Downloader";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label txtStatus;
+        private System.Windows.Forms.Button btnAbout;
+    }
+}
+
---
+++ YoutubeDownloader/Main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using VideoLibrary;
+using System.Threading;
+using System.Diagnostics;
+
+namespace YoutubeDownloader
+{
+    public partial class Main : Form
+    {
+        public Thread t;
+        public int length, counter;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveVideoToDisk(string link)
+        {
+            txtStatus.Invoke((MethodInvoker)(() =>
+            {
+                btnDownload.Enabled = false;
+            }));
+            var youTube = YouTube.Default; // starting point for YouTube actions
+            try
+            {
+                var video = youTube.GetVideo(link); // gets a Video object with info about the video
+
+                string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                filePath += @"\youtube\";
+
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+
+                File.WriteAllBytes(filePath + video.FullName, video.GetBytes());
+
+                txtStatus.Invoke((MethodInvoker)(() =>
+                {
+                    txtStatus.Text = $"{video.FullName} - done";
+                    GC.Collect();
+                    counter++;
+                    if (counter == length)
+                    {
+                        txtStatus.Text = "Finished.";
+                        btnDownload.Enabled = true;
+                        Process.Start(filePath);
+                    }
+                }));
+            }
+            catch (Exception e)
+            {
+                txtStatus.Invoke((MethodInvoker)(() =>
+                {
+                    MessageBox.Show(e.Message);
+                    txtStatus.Text = e.Message;
+                    btnDownload.Enabled = true;
+                    GC.Collect();
+                }));
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Created by: Jamuel Galicia\nIf you found bugs please report to me.");
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Text = "Youtube Downloader";
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string[] links = txtUrl.Text.Split('\n');
+            length = links.Length;
+            counter = 0;
+            Thread[] t = new Thread[length];
+            for (int x = 0; x < links.Length; x++)
+            {
+                string value = links[x];
+                t[x] = new Thread(() => SaveVideoToDisk($"{value}"));
+                t[x].Start();
+            }
+            txtStatus.Text = "Downloading started.";
+        }
+    }
+}
